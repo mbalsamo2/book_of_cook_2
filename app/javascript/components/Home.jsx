694269096f08c87e8f1e2523519d414c48f063c4
Mia Balsamo <mia.balsamo@gmail.com>
2020-09-03 22:08:00 -0400

    try to add homepage logo

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from './../../../public/the-book-of-cook.png';
 
 export default () => (
   <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
     <div className="jumbotron jumbotron-fluid bg-transparent">
       <div className="container secondary-color">
-        <h1 className="display-4">Food Recipes</h1>
+        <img
+          src={require(`./${logo}`)}
+          alt={'The Book of Cook'}
+        />
         <p className="lead">
-          A curated list of recipes for the best homemade meal and delicacies.
+          The virtual cookbook and recipe organizer.
         </p>
         <hr className="my-4" />
         <Link
